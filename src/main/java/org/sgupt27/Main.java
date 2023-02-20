fadc82d5bdee58bf96d1c2abe8ae04244709f7b9
Shirshaditya Gupta <shirshaditya.gupta@gmail.com>
2023-02-19 23:07:55 -0600

    Initial commit

---
+++ src/main/java/org/sgupt27/Main.java
@@ -0,0 +1,24 @@
+package org.sgupt27;
+
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("My SWT Window");
+        shell.open();
+
+        // Listen for IPC messages here and open a new SWT window when required.
+
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
+    }
+}
